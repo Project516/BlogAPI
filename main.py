0f308acbc2469d580134bf6f65ebfd0b1bc4a27b
project516 <138796702+Project516@users.noreply.github.com>
2026-04-21 02:32:58 +0000

    CORS

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 @app.get("/hello")
+
 def hello():
     return {"message": "Hello, World!"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 uvicorn.run(app)
