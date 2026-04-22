b100f2b3811c16ab820bad5095a12c179b75aa73
project516 <138796702+Project516@users.noreply.github.com>
2026-04-22 16:51:04 +0000

    add search function

--- main.py
+++ main.py
@@ -6,26 +6,32 @@ import json
 
 app = FastAPI()
 
+try:
+    with open("cache.json", "r") as file:
+        cache = json.load(file)
+except FileNotFoundError:
+    cache = []
+
+latest = cache[0] if cache else None
+
 
 @app.get("/blogs")
 def get_blogs():
-    return scrape_blogs(
-        "https://raw.githubusercontent.com/Project516/project516.github.io/refs/heads/master/blog.html"
-    )
+    return cache
 
 
 @app.get("/blogs/latest")
 def get_latest_blogs():
-    return scrape_blogs(
-        "https://raw.githubusercontent.com/Project516/project516.github.io/refs/heads/master/blog.html"
-    )
+    return latest
 
 
 @app.get("/blogs/search")
 def search_blogs(query: str):
-    return scrape_blogs(
-        "https://raw.githubusercontent.com/Project516/project516.github.io/refs/heads/master/blog.html"
-    )
+    results = []
+    for blog in cache:
+        if query.lower() in blog["title"].lower():
+            results.append(blog)
+    return results if results else {"message": "Blog not found"}
 
 
 @app.post("/blogs/cache")
