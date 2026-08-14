2ef1ebcbd14f5c4472aa0cac88826261e09abdc5
cappy-dev <cappy-dev@users.noreply.github.com>
2026-08-14 08:08:25 +0000

    Improve type hinting in main.py and scrape.py

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def scrape_blogs(url):
+def scrape_blogs(url: str) -> list[dict[str, str]]:
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
